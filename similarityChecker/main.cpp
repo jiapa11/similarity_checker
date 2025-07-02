73787a6414a2ab4ba8baae0819823ec100bf532a
noname <none@none.com>
2025-07-02 16:07:34 +0900

    Length checker implementation

--- similarityChecker/main.cpp
+++ similarityChecker/main.cpp
@@ -1,15 +1,26 @@
 #include "gmock/gmock.h"
 #include "similarity_checker.cpp"
 
-//class SimilarityCheckerFixture : public testing::Test {
-//public:
-//    
-//};
-//
-//TEST_F(SimilarityCheckerFixture, numOfLetters) {
-//    EXPECT_EQ(60, 60);
-//}
-//
+class SimilarityCheckerFixture : public testing::Test {
+public:
+    SimilarityChecker checker{ "ABC" };
+
+    void checkLengthSimilarity(int expectedScore, const std::string& target) {
+        int actual = checker.getLengthScore(target);
+        EXPECT_EQ(expectedScore, actual);
+    }
+
+};
+
+TEST_F(SimilarityCheckerFixture, length) {
+    checkLengthSimilarity(SimilarityChecker::MAX_LENGTH_SCORE, "ABC");
+    checkLengthSimilarity(0, "ABCABCA");
+    checkLengthSimilarity(40, "DEFE");
+    checkLengthSimilarity(0, "A");
+    checkLengthSimilarity(20, "ZZZXE");
+    checkLengthSimilarity(0, "");
+}
+
 //TEST_F(SimilarityCheckerFixture, letterMatch) {
 //    EXPECT_EQ(40, 40);
 //}
--- similarityChecker/similarity_checker.cpp
+++ similarityChecker/similarity_checker.cpp
@@ -1,5 +1,44 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+
+using std::string;
+
 class SimilarityChecker {
 public:
-private: 
+    static const int MAX_LENGTH_SCORE = 60;
 
+    SimilarityChecker(const std::string& source) : original{ source } {};
+    
+    int getLengthScore(const std::string& target) {
+        if (target == original) return MAX_LENGTH_SCORE;
+        if (isTargetStringEmpty(target)) return 0;
+        
+        double length_longer = original.length();
+        double length_shorter = target.length();
+        if (length_shorter > length_longer) std::swap(length_longer, length_shorter);
+
+        if (isLengthDiffMoreThanDouble(length_longer, length_shorter)) return 0;
+
+        return getPartialLengthScore(length_shorter, length_longer);
+    }
+
+private: 
+    const std::string original;
+    const std::string EMPTY_TARGET = "";
+
+    bool isTargetStringEmpty(const std::string& target)
+    {
+        return target == EMPTY_TARGET;
+    }
+
+    bool isLengthDiffMoreThanDouble(double length_longer, double length_shorter)
+    {
+        return (length_longer / length_shorter) >= 2;
+    }
+
+    double getPartialLengthScore(double length_shorter, double length_longer)
+    {
+        return ((length_shorter * 2 - length_longer) / length_shorter) * MAX_LENGTH_SCORE;
+    }
 };
