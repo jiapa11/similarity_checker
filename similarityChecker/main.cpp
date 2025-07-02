7fc2019587bd161fd817cda161bbb62357a4efbb
noname <none@none.com>
2025-07-02 17:10:31 +0900

    Implemented alphabet checker

--- similarityChecker/main.cpp
+++ similarityChecker/main.cpp
@@ -10,6 +10,16 @@ public:
         EXPECT_EQ(expectedScore, actual);
     }
 
+    void checkAlphabetSimilarity(int expectedScore, const std::string& str1, const std::string& str2) {
+        int actual = checker.getAlphabetScore(str1, str2);
+        EXPECT_EQ(expectedScore, actual);
+    }
+
+    void checkStringSimilarity(int expectedScore, const std::string& str1, const std::string& str2) {
+        int actual = checker.getScore(str1, str2);
+        EXPECT_EQ(expectedScore, actual);
+
+    }
 };
 
 TEST_F(SimilarityCheckerFixture, length) {
@@ -21,9 +31,25 @@ TEST_F(SimilarityCheckerFixture, length) {
     checkLengthSimilarity(0, "ABC", "");
 }
 
-//TEST_F(SimilarityCheckerFixture, letterMatch) {
-//    EXPECT_EQ(40, 40);
-//}
+TEST_F(SimilarityCheckerFixture, alpha_exactmatch) {
+    checkAlphabetSimilarity(SimilarityChecker::MAX_ALPHABET_SCORE, "ABC", "ABC");
+    checkAlphabetSimilarity(SimilarityChecker::MAX_ALPHABET_SCORE, "XYZE", "XYZE");
+}
+
+TEST_F(SimilarityCheckerFixture, alpha_zeromatch) {
+    checkAlphabetSimilarity(0, "XYZ", "ABC");
+    checkAlphabetSimilarity(0, "NNNAS", "POWYE");
+}
+
+TEST_F(SimilarityCheckerFixture, alpha_lowercase) {
+    checkAlphabetSimilarity(0, "abc", "ABC");
+    checkAlphabetSimilarity(26, "AbC", "ABC");
+}
+
+TEST_F(SimilarityCheckerFixture, alpha_normal) {
+    checkAlphabetSimilarity(20, "XYA", "ZXY");
+    checkAlphabetSimilarity(24, "ABCDE", "ABC");
+}
 
 int main() {
     ::testing::InitGoogleTest();
--- similarityChecker/similarity_checker.cpp
+++ similarityChecker/similarity_checker.cpp
@@ -4,16 +4,26 @@
 
 using std::string;
 
+typedef int bitmap;
+
 class SimilarityChecker {
 public:
     static const int MAX_LENGTH_SCORE = 60;
+    static const int MAX_ALPHABET_SCORE = 40;
 
     SimilarityChecker() = default;
-    
+
+    int getScore(const std::string& str1, const std::string& str2) {
+        if (str1 == str2) return MAX_LENGTH_SCORE + MAX_ALPHABET_SCORE;
+        if (hasEmptyString(str1, str2)) return 0;
+        
+        return getLengthScore(str1, str2) + getAlphabetScore(str1, str2);
+    }
+
     int getLengthScore(const std::string& str1, const std::string& str2) {
         if (str1 == str2) return MAX_LENGTH_SCORE;
         if (hasEmptyString(str1, str2)) return 0;
-        
+
         size_t length_longer = str1.length();
         size_t length_shorter = str2.length();
         if (length_shorter > length_longer) std::swap(length_longer, length_shorter);
@@ -23,8 +33,22 @@ public:
         return getPartialLengthScore(length_shorter, length_longer);
     }
 
-private: 
+    int getAlphabetScore(const std::string& str1, const std::string& str2) {
+        if (str1 == str2) return MAX_ALPHABET_SCORE;
+        if (hasEmptyString(str1, str2)) return 0;
+
+        int totalCnt = getTotalAlphabetCount(str1, str2);
+        int sameCnt = getSameAlphabetCount(str1, str2);
+
+        if (totalCnt == 0 || sameCnt == 0) return 0;
+
+        return getPartialAlphabetScore(totalCnt, sameCnt);
+    }
+
+
+private:
     const std::string EMPTY_STRING = "";
+    static const int NUM_OF_ALPHABET = 30; // 26 + @
 
     bool hasEmptyString(const std::string& str1, const std::string& str2)
     {
@@ -36,10 +60,64 @@ private:
         return (length_longer / length_shorter) >= 2;
     }
 
-    double getPartialLengthScore(size_t length_shorter, size_t length_longer)
+    int getPartialLengthScore(size_t length_shorter, size_t length_longer)
     {
         double shorter = static_cast<double>(length_shorter);
         double longer = static_cast<double>(length_longer);
-        return ((shorter * 2 - longer) / shorter) * MAX_LENGTH_SCORE;
+        double score = ((shorter * 2 - longer) / shorter) * MAX_LENGTH_SCORE;
+
+        return static_cast<int>(score);
+    }
+
+    int getPartialAlphabetScore(int totalCnt, int sameCnt)
+    {
+        double percentage = static_cast<double>(sameCnt) / static_cast<double>(totalCnt);
+        double score = static_cast<double>(MAX_ALPHABET_SCORE) * percentage;
+
+        return static_cast<int>(score);
+    }
+
+    int getTotalAlphabetCount(const std::string& str1, const std::string& str2) {
+        bool alphabetInString[NUM_OF_ALPHABET] = { false };
+        int count = 0;
+
+        for (const char& ch : str1) {
+            if (ch >= 'A' && ch <= 'Z') {
+                if (alphabetInString[ch - 'A'] == true) continue;
+                alphabetInString[ch - 'A'] = true;
+                count++;
+            }
+        }
+
+        for (const char& ch : str2) {
+            if (ch >= 'A' && ch <= 'Z') {
+                if (alphabetInString[ch - 'A'] == true) continue;
+                count++;
+            }
+        }
+
+        return count;
+    }
+
+    int getSameAlphabetCount(const std::string& str1, const std::string& str2) {
+        bool alphabetInString[NUM_OF_ALPHABET] = { false };
+        int count = 0;
+
+        for (const char& ch : str1) {
+            if (ch >= 'A' && ch <= 'Z') {
+                if (alphabetInString[ch - 'A'] == true) continue;
+                alphabetInString[ch - 'A'] = true;
+            }
+        }
+
+        for (const char& ch : str2) {
+            if (ch >= 'A' && ch <= 'Z') {
+                if (alphabetInString[ch - 'A'] == true) {
+                    count++;
+                }
+            }
+        }
+
+        return count;
     }
 };
